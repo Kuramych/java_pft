039130339ea8eb17bbbf231bac918b6299cb7e58
My Name <vvmelnichenko@miem.hse.ru>
2023-07-29 16:40:50 +0300

    ДЗ 1.2 1.3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,11 +11,18 @@ public class MyFirstProgram {
 
         Rectangle r = new Rectangle(4,6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+        Point p1 = new Point(3,4);
+        Point p2 = new Point(5,6);
+        System.out.println("Расстояние между двумя точками с координатами " + p1.p1 + "," + p1.p2 + " и " + p2.p1 + "," + p2.p2 + " = " + distance(p1,p2));
     }
 
     public static void hello(String somebody){
         System.out.println("Hello, " + somebody + "!");
     }
 
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt(Math.pow(p2.p1 - p1.p1, 2) + Math.pow(p2.p2 - p1.p2, 2));
+    }
 
 }
