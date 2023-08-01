344ad0d4895a51028aab13e35af96a01866248b4
My Name <vvmelnichenko@miem.hse.ru>
2023-08-01 14:03:33 +0300

    Добавлен браузер Firefox для запуска

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,20 +2,35 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 
 import java.time.Duration;
+import java.util.Objects;
 
 public class ApplicationManager {
-    WebDriver wd;
+        private Browser browser;
+        WebDriver wd;
 
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
     private ContactHelper contactHelper;
 
+    public ApplicationManager(Browser browser) {
+        this.browser = browser;
+    }
+
     public void init() {
-        System.setProperty("webdriver.chrome.driver", "C://Windows//System32//0409//chromedriver.exe");
-        wd = new ChromeDriver();
+        if (Objects.equals(browser, Browser.CHROME)) {
+            System.setProperty("webdriver.chrome.driver", "C://Windows//System32//0409//chromedriver.exe");
+            wd = new ChromeDriver();
+        } else if (Objects.equals(browser, Browser.FIREFOX)) {
+            System.setProperty("webdriver.gecko.driver", "C://Windows//System32//0409//geckodriver.exe");
+            System.setProperty("webdriver.firefox.bin", "C://Program Files//Mozilla Firefox//firefox.exe");
+            wd = new FirefoxDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
