745126804ecbd334554ad3764a17bb86ee425dc2
My Name <vvmelnichenko@miem.hse.ru>
2023-08-06 19:59:57 +0300

    Данные для теста создания контакта загружаются из файла contacts.csv Тест стал параметризованный

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,8 +3,15 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.*;;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -12,6 +19,30 @@ import static org.hamcrest.MatcherAssert.*;
 import static org.testng.Assert.*;
 
 public class ContactCreationTests extends TestBase{
+
+    @DataProvider
+    public Iterator<Object[]> validContacts() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1]).withPhoto(new File(split[2]))});
+            line = reader.readLine();
+        }
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void addNewContactTest(ContactData contact) {
+        app.goTo().contactPage();
+        Contacts before = app.contact().all();
+        app.contact().create(contact);
+        assertThat(app.contact().getContactCount(), equalTo(before.size() + 1));
+        Contacts after = app.contact().all();
+        assertThat(after, equalTo(
+                before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    }
 
     @Test(enabled = false)
     public void addNewContactTest1() {
@@ -24,18 +55,5 @@ public class ContactCreationTests extends TestBase{
         contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
         before.add(contact);
         assertEquals(before, after);
-    }
-
-    @Test
-    public void addNewContactTest() {
-        app.goTo().contactPage();
-        Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/photo.jpg");
-        ContactData contact = new ContactData().withFirstname("test1").withLastname("test2").withPhoto(photo);
-        app.contact().create(contact);
-        assertThat(app.contact().getContactCount(), equalTo(before.size() + 1));
-        Contacts after = app.contact().all();
-        assertThat(after, equalTo(
-                before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 }
